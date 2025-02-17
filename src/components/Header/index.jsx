4e7b836e229334bbc4963cc46faac1aa1e71df10
Endorze <alexyurihallgren@hotmail.com>
2025-02-17 12:14:13 +0100

    init commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>I'm a lvl1 header and im called Index for some goddamn reason</h1>
+                <h2>First component built with Rob</h2>
+            </header>
+        </>
+    )
+}
+
+export default Header;
