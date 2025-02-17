749699a55abdf4d0957c996dd19f30e58d6b4936
Endorze <alexyurihallgren@hotmail.com>
2025-02-17 15:24:36 +0100

    Fix: added images to articles.

--- src/components/ArticleSection/articleSection.jsx
+++ src/components/ArticleSection/articleSection.jsx
@@ -1,24 +1,29 @@
 import styles from "./articleSection.module.css"
+import img from "../../assets/react.svg"
 
 const ArticleSection = () => {
     return (
         <div>
             <div className={styles.horizontalBoxWrapper}>
             <div className={styles.articleBox}>
+                <img src={img}/>
                 <h4>Article</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit!</p>
             </div>
             <div className={styles.articleBox}>
+                <img src={img}/>
                 <h4>Article</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit!</p>
             </div>
             </div>
             <div className={styles.horizontalBoxWrapper}>
             <div className={styles.articleBox}>
+                <img src={img}/>
                 <h4>Article</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit!</p>
             </div>
             <div className={styles.articleBox}>
+                <img src={img}/>
                 <h4>Article</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit!</p>
             </div>
